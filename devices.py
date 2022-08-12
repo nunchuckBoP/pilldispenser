9a509ddfbad1f47878ab2ae5c1a2f8f912ebaec9
nunchuckbop <cj@neechsoft.com>
2022-08-12 12:27:38 -0500

    added comment

--- devices.py
+++ devices.py
@@ -2,11 +2,12 @@
 # i2c load cell using adafruit adc
 # https://www.adafruit.com/product/4538
 from cedargrove_nau7802 import NAU7802
-
 class LoadCell(object):
     pass
 
 
+# pill motor and pump are controlled via an
+# adafruit motor control hat
 class PillMotor(object):
     pass
 
