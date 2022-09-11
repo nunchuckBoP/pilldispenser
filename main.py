ebfda615688393d30f4787f355781ad14d5a29f1
nunchuckbop <cj@neechsoft.com>
2022-09-11 12:47:57 -0500

    debugging

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from fsm import PillDispenser
+import time
 
 if __name__ == '__main__':
 
-        print("Begining!")
+        # instantiate the state machine
+        fsm = PillDispenser("1")
 
-        # do a bunch of stuff
-        print("Doing a lot of work...")
+        # wake up the state machine
+        fsm.wake_up()
 
+        # keeps track of the last state
+        last_state = None
+
+        # start comparing states
+        while True:
+
+                # This if statement prints out the state every time the 
+                # state of the machine changes.
+                if last_state != fsm.state:
+                        print("Current State: %s" % fsm.state)
+                        last_state = fsm.state
+                # end if
+
+        # end while
+        
         print("System Completed.")
